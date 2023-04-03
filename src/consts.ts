1513114378983516e27664c9612fea566effe303
Kevin Sparling <kevinsparling@gmail.com>
2023-04-03 20:34:32 +0800

    initial commit

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'QSI EL Week';
+export const SITE_DESCRIPTION = 'Plan EL Week 2024';
